b5e84ac1c48cc8d470d3b61f95c390c7cd4bc76c
ekosaputro45 <bontoloyo@gmail.com>
2024-11-04 12:44:00 +0700

    Create larihan18.java

---
+++ larihan18.java
@@ -0,0 +1,13 @@
+
+package pertemuan6;
+
+public class latihan18 {
+   
+    public static void main(String[] args) {
+        System.out.println("Bilangan kelipatan 3,5 dari 3,5 hingga 35:");
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.printf("%.1f%n", i);
+        }
+    }
+
+}
